3817ec3eb918735a784aa7cba6d9e729249f542e
mquinlan <m.quinlan81@gmail.com>
2022-12-17 19:21:06 -0600

    commit on rps-ui

--- rps-js.js
+++ rps-js.js
@@ -5,6 +5,8 @@ let gameBoard = document.querySelector('#game-board')
 let computer = document.querySelector('#computer')
 let player = document.querySelector('#player')
 
+
+
 function getComputerChoice() {
    const randomNumber = Math.floor((Math.random() * 3));
    console.log(`Random Number: ${randomNumber}`);
@@ -32,15 +34,17 @@ function getWinner(userChoice, computerChoice) {
 }
  
 function playRound(userChoice, computerChoice) {
+
     let result = getWinner(userChoice, computerChoice);
   
     if (result === "Player") {
         const pPick = document.createElement('p');
-        pPick.classList.add('winner');
+        pPick.style.cssText = 'font-weight: bold; scale: 1.2';
         pPick.textContent = `${userChoice}`;
         player.appendChild(pPick);
 
         const cPick = document.createElement('p');
+        cPick.style.cssText = 'color: #444';
         cPick.textContent = `${computerChoice}`;
         computer.appendChild(cPick);
 
@@ -50,11 +54,12 @@ function playRound(userChoice, computerChoice) {
 
     } else if (result === "Computer") {
         const pPick = document.createElement('p');
+        pPick.style.cssText = 'color: #444';
         pPick.textContent = `${userChoice}`;
         player.appendChild(pPick);
 
         const cPick = document.createElement('p');
-        cPick.classList.add('winner');
+        cPick.style.cssText = 'font-weight: bold; scale: 1.2';
         cPick.textContent = `${computerChoice}`;
         computer.appendChild(cPick);
 
@@ -77,39 +82,35 @@ function playRound(userChoice, computerChoice) {
     }
 }
 
+function keepScore () {
+    let playerScore = 0;
+    let compScore = 0;
+
+        if (getWinner(userChoice, computerChoice) == "Player") {
+            playerScore++
+        } else if (getWinner(userChoice, computerChoice) == "Computer") {
+            compScore++
+        } 
+
+    console.log(playerScore);
+    console.log(compScore);
+
+}
+
 rock.addEventListener('click', () => {
     const userChoice = rock.id;
     const computerChoice = getComputerChoice();
-    console.log(playRound(userChoice, computerChoice));
+    playRound(userChoice, computerChoice);
 });
 
 paper.addEventListener('click', () => {
   const userChoice = paper.id;
   const computerChoice = getComputerChoice();
-  console.log(playRound(userChoice, computerChoice));
+  playRound(userChoice, computerChoice);
 });
 
 scissors.addEventListener('click', () => {
   const userChoice = scissors.id;
   const computerChoice = getComputerChoice();
-  console.log(playRound(userChoice, computerChoice));
+  playRound(userChoice, computerChoice);
 });
-
-//    let playerScore = 0;
-//    let compScore = 0;
-//    for (let i = 0; i < 5; i++){
-
-//        if (getWinner(userChoice, computerChoice) == "Player") {
-//            playerScore++
-//        } else if (getWinner(userChoice, computerChoice) == "Computer") {
-//            compScore++
-//        }
-//    }
-//    console.log("GAME OVER");
-//    if (playerScore > compScore) {
-//        console.log(`Player Wins! ${playerScore} - ${compScore}`)
-//    } else if (playerScore < compScore) {
-//        console.log(`Computer Wins! ${compScore} - ${playerScore}`)
-//    } else {
-//        console.log(`It's a Tie! ${compScore} - ${playerScore}`)
-//    }
